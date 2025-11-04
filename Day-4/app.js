64d59d5618ab1ec339ad0e85dbc1d4eb81459d5f
vzl-rbi <screwup39@gmail.com>
2025-11-04 14:43:06 +0545

    first commit

---
+++ Day-4/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+const PORT = 8000;
+app.get("/", (req, res) => {
+  res.send("<h1> This is Home Page</h1>");
+});
+app.get("/about", (req, res) => {
+  res.send("<h2> About Page</h2>");
+});
+
+app.listen(PORT, () =>
+  console.log(`Project has started at PORT http://localhost:${PORT}`)
+);
