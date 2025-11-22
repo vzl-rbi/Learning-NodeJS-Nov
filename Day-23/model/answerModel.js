36318ada911486349ee6f2367cc962d681a2ee52
vzl-rbi <screwup39@gmail.com>
2025-11-22 09:55:42 +0545

    Day-23 : Learned to implement socket io for like Feature for answer

---
+++ Day-23/model/answerModel.js
@@ -0,0 +1,13 @@
+export const answerModel = (sequelize, DataTypes) => {
+  const Answer = sequelize.define("answer", {
+    answerText: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  });
+  return Answer;
+};
